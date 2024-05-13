5e9f03e614058a17f4dc99f02eaad526f78a1baf
abolfazlf56 <farajif56@gmail.com>
2024-05-14 01:13:14 +0330

    t

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandGUI.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoenchants/commands/CommandGUI.kt
@@ -3,12 +3,21 @@ package com.willfp.ecoenchants.commands
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.ecoenchants.enchant.EnchantGUI
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 
-class CommandGUI(plugin: EcoPlugin) : Subcommand(plugin, "gui", "ecoenchants.command.gui", true) {
-    override fun onExecute(player: CommandSender, args: List<String>) {
-        player as Player
+class CommandGUI(plugin: EcoPlugin) : Subcommand(plugin, "gui", "ecoenchants.command.gui", false) {
+    override fun onExecute(sender: CommandSender, args: List<String>) {
+        val playerName = args.getOrNull(0)
+        if (playerName == null) {
+            sender.sendMessage(plugin.langYml.getMessage("requires-player"))
+            return
+        }
+        val player = Bukkit.getPlayer(playerName)
+        if (player == null) {
+            sender.sendMessage(plugin.langYml.getMessage("invalid-player"))
+            return
+        }
         EnchantGUI.openGUI(player)
     }
 }
